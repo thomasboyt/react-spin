199bb9d9a786eb5fd360c437b3e759e10482fa04
Thomas Boyt <thomas@ledgerx.com>
2014-09-23 16:32:53 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,36 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var Spinner = require('spin.js');
+
+var ReactSpinner = React.createClass({
+  propTypes: {
+    config: React.PropTypes.object,
+    stopped: React.PropTypes.bool
+  },
+
+  componentDidMount: function() {
+    this.spinner = new Spinner(this.props.config);
+    this.spinner.spin(this.refs.container.getDOMNode());
+  },
+
+  componentWillReceiveProps: function(newProps) {
+    if (newProps.stopped === true && !this.props.stopped) {
+      this.spinner.stop();
+    } else if (!newProps.stopped && this.props.stopped === true) {
+      this.spinner.spin();
+    }
+  },
+
+  componentWillUnmount: function() {
+    this.spinner.stop();
+  },
+
+  render: function() {
+    return (
+      <span ref="container" />
+    );
+  }
+});
+
+module.exports = ReactSpinner;
