f7bc7f8566bd5c90bd9d2b4409274c0f75cfe1fe
paul <paul.christophe6@gmail.com>
2017-06-28 19:14:42 -0400

    running an old version of firefox in travis which doesn't recognize "class"

--- src/__tests__/main.spec.js
+++ src/__tests__/main.spec.js
@@ -1,6 +1,6 @@
 import expect from 'expect';
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-dom/test-utils';
 
 import ReactSpinner from '../main';
 
